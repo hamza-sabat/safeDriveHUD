f8c4c583bdacf0535a3bd81b796bbe4607ac9e0b
KhateebADA <117444434+KhateebADA@users.noreply.github.com>
2024-08-09 12:42:16 +0100

    bug fixes- server file location changes

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the current directory
-app.use(express.static('.'));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Setup a basic route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Set the server to listen on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
